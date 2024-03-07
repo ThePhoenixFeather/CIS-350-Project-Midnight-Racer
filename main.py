46e1e2ad5406a98c3dff168c9fafe0bf59d3ebf5
ThePhoenixFeather <159597298+ThePhoenixFeather@users.noreply.github.com>
2024-03-07 15:31:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import pygame
 
 if __name__ == '__main__':
-    print('done')
+    # Add pygame implementation later
+    choice = 0
+    while choice != 4:
+        
+        print('Please enter a number to play (1), check scoreboard (2), go to settings (3), or quit (4): ')
+        input(choice)
+        while choice > 4 or choice < 1:
+            print('Invalid input, please reenter')
+        
+        if choice == 1:
+            # game section here
+        
+        if choice == 2:
+            # scoreboard section here
+        
+        if choice == 3:
+            # settings section here
