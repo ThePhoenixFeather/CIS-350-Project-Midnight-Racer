d6fcd2f8c09a3e8ac5554f16dab741009da52acc
ThePhoenixFeather <159597298+ThePhoenixFeather@users.noreply.github.com>
2024-03-07 15:33:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,30 @@ class Game:
 
 
 # Scoreboard class
+class Scoreboard:
+    currScore = 0
+    currTime = 0
+    currAccel = 0
 
+    savedScore_1 = 0
+    savedScore_2 = 0
+    savedScore_3 = 0
+    savedScore_4 = 0
+    savedScore_5 = 0
+
+    savedTime_1 = 0
+    savedTime_2 = 0
+    savedTime_3 = 0
+    savedTime_4 = 0
+    savedTime_5 = 0
+
+    savedAccel_1 = 0
+    savedAccel_2 = 0
+    savedAccel_3 = 0
+    savedAccel_4 = 0
+    savedAccel_5 = 0
+
+# Settings Class
 
 
 if __name__ == '__main__':
